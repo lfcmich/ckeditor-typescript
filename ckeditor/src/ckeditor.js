bae872f27ae0f9aa2f034354a48125c084a1faa5
Michael Chung <mich@ccinformation.com>
2026-01-20 15:54:10 +0800

    bump version to 47.4

--- ckeditor/src/ckeditor.js
+++ ckeditor/src/ckeditor.js
@@ -2,42 +2,43 @@
  * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js'
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js'
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js'
-import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js'
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js'
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js'
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js'
-import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js'
-import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js'
-import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js'
-import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js'
-import Heading from '@ckeditor/ckeditor5-heading/src/heading.js'
-import Image from '@ckeditor/ckeditor5-image/src/image.js'
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js'
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js'
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js'
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js'
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js'
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js'
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js'
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js'
-import Link from '@ckeditor/ckeditor5-link/src/link.js'
-import List from '@ckeditor/ckeditor5-list/src/list.js'
-import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js'
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js'
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js'
-import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js'
-import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js'
-import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js'
-import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js'
-import Table from '@ckeditor/ckeditor5-table/src/table.js'
-import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties'
-import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties'
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js'
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js'
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js'
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js'
+import { Alignment } from '@ckeditor/ckeditor5-alignment/src/alignment.js'
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat/src/autoformat.js'
+import { AutoImage } from '@ckeditor/ckeditor5-image/src/autoimage.js'
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote/src/blockquote.js'
+import { Bold } from '@ckeditor/ckeditor5-basic-styles/src/bold.js'
+import { Essentials } from '@ckeditor/ckeditor5-essentials/src/essentials.js'
+import { FontBackgroundColor } from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js'
+import { FontColor } from '@ckeditor/ckeditor5-font/src/fontcolor.js'
+import { FontSize } from '@ckeditor/ckeditor5-font/src/fontsize.js'
+import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js'
+import { Heading } from '@ckeditor/ckeditor5-heading/src/heading.js'
+import { Image } from '@ckeditor/ckeditor5-image/src/image.js'
+import { ImageCaption } from '@ckeditor/ckeditor5-image/src/imagecaption.js'
+import { ImageResize } from '@ckeditor/ckeditor5-image/src/imageresize.js'
+import { ImageStyle } from '@ckeditor/ckeditor5-image/src/imagestyle.js'
+import { ImageToolbar } from '@ckeditor/ckeditor5-image/src/imagetoolbar.js'
+import { ImageUpload } from '@ckeditor/ckeditor5-image/src/imageupload.js'
+import { Indent } from '@ckeditor/ckeditor5-indent/src/indent.js'
+import { IndentBlock } from '@ckeditor/ckeditor5-indent/src/indentblock.js'
+import { Italic } from '@ckeditor/ckeditor5-basic-styles/src/italic.js'
+import { Link } from '@ckeditor/ckeditor5-link/src/link.js'
+import { List } from '@ckeditor/ckeditor5-list/src/list.js'
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js'
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js'
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph/src/paragraph.js'
+import { SimpleUploadAdapter} from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js'
+import { SourceEditing } from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js'
+import { Subscript } from '@ckeditor/ckeditor5-basic-styles/src/subscript.js'
+import { Superscript } from '@ckeditor/ckeditor5-basic-styles/src/superscript.js'
+import { Table } from '@ckeditor/ckeditor5-table/src/table.js'
+import { TableCellProperties } from '@ckeditor/ckeditor5-table/src/tablecellproperties'
+import { TableProperties } from '@ckeditor/ckeditor5-table/src/tableproperties'
+import { TableToolbar } from '@ckeditor/ckeditor5-table/src/tabletoolbar.js'
+import { TextTransformation } from '@ckeditor/ckeditor5-typing/src/texttransformation.js'
+import { Underline } from '@ckeditor/ckeditor5-basic-styles/src/underline.js'
+import { AutoLink } from '@ckeditor/ckeditor5-link/src/autolink.js'
 import { COLORS } from './color'
 
 class Editor extends ClassicEditor {}
@@ -63,6 +64,7 @@ Editor.builtinPlugins = [
   ImageUpload,
   Indent,
   IndentBlock,
+  AutoLink,
   Italic,
   Link,
   List,
@@ -83,6 +85,7 @@ Editor.builtinPlugins = [
 
 // Editor configuration.
 Editor.defaultConfig = {
+  licenseKey: 'GPL',
   toolbar: {
     items: [
       'heading',
@@ -116,6 +119,29 @@ Editor.defaultConfig = {
   language: 'en',
   image: {
     toolbar: ['imageTextAlternative', 'imageStyle:inline', 'imageStyle:block', 'imageStyle:side']
+  },
+  link: {
+    addTargetToExternalLinks: true,
+    toolbar: [ 'linkPreview', '|', 'editLink', 'linkProperties', 'unlink' ],
+    decorators: {
+      openInNewTab: {
+        mode: 'manual',
+        label: 'Open in a new tab',
+        defaultValue: false,
+        attributes: {
+          target: '_blank',
+          rel: 'noopener noreferrer'
+        }
+      },
+      downloadable: {
+        mode: 'manual',
+        label: 'Downloadable',
+        defaultValue: false,
+        attributes: {
+          download: 'download'
+        }
+      }
+    }
   },
   fontSize: {
     options: [10, 12, 'default', 14, 16, 18, 20]
